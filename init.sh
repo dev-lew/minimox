fa1332de34cca604ce6d391f9482a231c8e80984
dev-lew <55928726+dev-lew@users.noreply.github.com>
2026-08-17 16:31:53 -0400

    Update

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-ISO_VERSION="9.1-1"
-
 die() {
     case $? in
 	0)
@@ -9,6 +7,7 @@ die() {
 	    ;;
 	*)
 	    exitval=$?
+            ;;
     esac
 
     printf '%s: %s\n' "${0##*/}" "$*" >&2
@@ -16,7 +15,19 @@ die() {
     exit "${exitval}"
 }
 
-download_iso() {
-    wget "https://enterprise.proxmox.com/iso/proxmox-ve_${ISO_VERSION}.iso" ||
-        die "Failed to download Proxmox VE version ${ISO_VERSION}"
+provision_ansible_user() {
+    if ! pveum user list | grep -qx 'ansible@pve'; then
+        pveum user add ansible@pve \
+            || die "failed to create ansible user"
+    fi
+
+    pveum aclmod / \
+        -user ansible@pve \
+        -role Administrator \
+        || die "failed to grant Administrator role"
+
+    if ! pveum user token list ansible@pve | grep -q 'ansible'; then
+        pveum user token add ansible@pve ansible --output-format json \
+            || die "failed to create API token"
+    fi
 }
